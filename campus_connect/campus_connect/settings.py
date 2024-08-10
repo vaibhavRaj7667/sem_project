4fc840bf36041538be8b8e3ad5cef73d7dc1936f
vaibhavRaj7667 <zivaibhav1@gmail.com>
2024-08-10 19:31:21 +0530

    work left

--- campus_connect/campus_connect/settings.py
+++ campus_connect/campus_connect/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'campus_connect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#change
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#change
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- campus_connect/student/models.py
+++ campus_connect/student/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class questions(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    question_text = models.TextField(max_length=240)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
