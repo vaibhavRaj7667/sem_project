50471df9d73687f516b9c6b1d85ff222323691ef
vaibhavRaj7667 <zivaibhav1@gmail.com>
2024-08-13 11:12:31 +0530

    add delete funct

--- campus_connect/student/urls.py
+++ campus_connect/student/urls.py
@@ -6,4 +6,7 @@ urlpatterns=[
     path('',views.login,name='login'),
     path('register/',views.register,name='register'),
     path('profile/',views.profile,name='profile'),
+    path('delete/<int:question_id>/', views.delete_question, name='delete_question'),
+    path('logout/', views.logout_view, name='logout'),
+
 ]
--- campus_connect/student/views.py
+++ campus_connect/student/views.py
@@ -7,11 +7,13 @@ from django.contrib import messages
 from .forms import QuestionForm
 from django.contrib.auth.decorators import login_required
 from .models import questions
+from django.shortcuts import render, redirect , get_object_or_404
+from django.contrib.auth import logout
 
 # Create your views here.
-def home(request):
-    all_questions = questions.objects.all()
-    return render(request,'home.html',{'all_questions': all_questions})
+# def home(request):
+#     all_questions = questions.objects.all()
+#     return render(request,'home.html',{'all_questions': all_questions})
 
 def login(request):
     if request.method=='POST':
@@ -26,6 +28,15 @@ def login(request):
             return redirect('login')
     else:
         return render(request,'login_page.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+def home(request):
+    all_questions = questions.objects.all()
+    return render(request,'home.html',{'all_questions': all_questions})
 
 def register(request):
     if request.method=='POST':
@@ -50,3 +61,14 @@ def profile(request):
         form = QuestionForm()
 
     return render(request,'profile.html',{'form':form})
+
+@login_required
+def delete_question(request, question_id):
+    question = get_object_or_404(questions, id=question_id, user=request.user)
+    if request.method == "POST":
+        question.delete()
+        return redirect('home')
+        
+    return render(request, 'delete_question.html', {'question': question})
+        
+    
